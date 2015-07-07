10eb633ab6d23759f5282bf6e30a5c8cac508ae4
CrazyLittleC <248348907@qq.com>
2015-07-07 20:31:17 +0800

    modified

--- EverydayWork/src/day02/TriglePrint.java
+++ EverydayWork/src/day02/TriglePrint.java
@@ -1,12 +1,16 @@
 package day02;
 
+import java.util.Scanner;
+
 public class TriglePrint {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		char[] ch = { '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
-				'C', 'D' };
-		for (int i = 0; i < ch.length; ++i) {
-			printSpace(2*ch.length-i);
-			printChar(ch[i], i+1);
+				'C', 'D' ,'E','F'};
+		for (int i = 0; i < n; ++i) {
+			printSpace(n-i);
+			printChar(ch[i], 2*i+1);
 		}
 
 	}
@@ -19,7 +23,7 @@ public class TriglePrint {
 	public static void printChar(char ch,int n)
 	{
 		for(int i=0;i<n;++i)
-			System.out.print(ch+" ");
+			System.out.print(ch);
 		System.out.println();
 	}
 }
